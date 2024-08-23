1ab44cc4291e0a2bd76aa6610b5d790f7a0e5a86
wendy97531234 <126539753+wendy97531234@users.noreply.github.com>
2024-08-22 19:56:14 -0600

    Guias<5

---
+++ DataSourceDemo/DataSourceDemo/ejemploConexion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSourceDemo
+{
+    public partial class ejemploConexion : Form
+    {
+        public ejemploConexion()
+        {
+            InitializeComponent();
+        }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        private void ejemploConexion_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'northwindDataSet.Customers' Puede moverla o quitarla según sea necesario.
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Llamamos a customerBindingSource y el metodo que queremos ejecutar
+            customersBindingSource.AddNew();
+        }
+
+        private void cajaTextoID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // MessageBox.Show("Ejemplo de funcionalidad");
+
+            if (e.KeyChar == (char)13)
+            {
+                var index = customersBindingSource.Find("customerID", cajaTextoID);
+                if (index > -1)
+                {
+                    customersBindingSource.Position = index;
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Elemento no encontrado");
+                }
+            }
+        }
+    }
+}
